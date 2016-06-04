a8b037f99de942e2dd58d91fe73b9544fcffea5d
starkwang <381152119@qq.com>
2016-06-04 23:26:03 +0800

    code

--- js/lineChartDrawer.js
+++ js/lineChartDrawer.js
@@ -1,7 +1,7 @@
 (function() {
     var myChart = echarts.init(document.getElementById('linechart'));
     // 指定图表的配置项和数据
-    function draw(data) {
+    function draw(trueRadius, data) {
         console.log(data);
         var d = [];
         for (var i = 0; i < 1000; i++) {
@@ -9,24 +9,51 @@
             var lose = (1000 - i) / 1000;
             d.push([deepth, lose]);
         }
+        var xMax = data[999] > trueRadius ? trueRadius.toString() : 'auto';
         var option = {
             title: {
-                text: '动态数据 + 时间坐标轴'
+                text: '电磁波在良导体中的衰减',
+                show: true
             },
             xAxis: {
+                name: '深度（mm）',
+                nameLocation: 'middle',
+                nameGap: 20,
                 type: 'value',
+                max: xMax
             },
             yAxis: {
+                name: '衰减倍数',
                 type: 'value',
             },
+            clipOverflow: false,
             series: [{
                 name: '模拟数据',
                 type: 'line',
                 showSymbol: false,
-                hoverAnimation: false,
-                data: d
+                data: d,
+                markLine: {
+                    data: [
+                        [{
+                            symbol: 'arrow',
+                            label: {
+                                normal: {
+                                    formatter: '趋肤深度：' + data[632].toFixed(5) + 'mm'
+                                }
+                            },
+                            name: '趋肤深度：' + data[632].toFixed(5) + 'mm',
+                            coord: d[632]
+                        }, {
+                            symbol: 'circle',
+                            x: '50%',
+                            y: '50%'
+                        }]
+                    ],
+                    animation: false
+                }
             }]
         };
+        console.log(d[632]);
         myChart.setOption(option);
     }
     // 使用刚指定的配置项和数据显示图表。
--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
-$("#frequency").change(function(){
-    var f = $(this).val();
-    var data = crossSectionDrawer.calculate(6.17, f);
-    crossSectionDrawer.draw(100, 0.05, data);
-    lineChartDrawer.draw(data)
-})
+var data = crossSectionDrawer.calculate(6.17, 5);
+crossSectionDrawer.draw(100, 0.5, data);
+lineChartDrawer.draw(0.5, data);
 
+$(".control").change(function() {
+    var f = $("#frequency").val();
+    var electricConductivity = $('#electricConductivity').val() || 6.17; //电导率
+    var trueRadius = $("#trueRadius").val();
+    var data = crossSectionDrawer.calculate(electricConductivity, f);
+    crossSectionDrawer.draw(100, trueRadius, data);
+    lineChartDrawer.draw(trueRadius, data)
+});
+
+$(document).ready(function() {
+    $('select').material_select();
+});
